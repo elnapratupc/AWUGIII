b44d2095b0bcee11aa2ef2dfcd0a851a3c169bba
Elna Prat (UPC) <elna.prat@estudiantat.upc.edu>
2025-04-07 11:51:48 +0200

    borrat arxius expo, plantilla neta

--- morfilm/App.js
+++ morfilm/App.js
@@ -1,18 +1,13 @@
-// App.js
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView } from 'react-native';
+import HomeScreen from './screens/homescreen';
 
-import LoginScreen from './screens/loginscreen';
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
+const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView>
+      <HomeScreen />
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
--- morfilm/screens/homescreen.js
+++ morfilm/screens/homescreen.js
@@ -1,1 +1,13 @@
 //https://www.figma.com/design/LGJmOwBrURlk4xYNH15mmX/Prototip-AWUGIII?node-id=19-14485&t=y3FzRbd2wninaYHL-4
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Hola, aquest és el teu projecte personalitzat!</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
